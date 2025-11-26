a5c685a03901c8900ecf1cebaf9e24b532616f64
GuPune <92207039+GuPune@users.noreply.github.com>
2025-11-26 09:56:11 +0700

    Update index.blade.php

--- resources/views/departmentsUser/index.blade.php
+++ resources/views/departmentsUser/index.blade.php
@@ -45,8 +45,26 @@
               </a>
             </td>
             <td class="px-4 py-2">{{ $dep->name }}</td>
-             <td class="px-4 py-2">{{ $dep->branches->brn_id }}</td>
-            <td class="px-4 py-2">{{ $dep->branches->name }}</td>
+                        <td class="px-4 py-2" >
+           
+
+              @if($dep->branches)   
+                 {{ $dep->branches->brn_id }}      
+@else
+ - 
+@endif
+            </td>
+            <td class="px-4 py-2">
+
+              @if($dep->branches)   
+                 {{ $dep->branches->name }}      
+@else
+ - 
+@endif
+
+            </td>
+
+            
             <td class="px-4 py-2">{{ $dep->updated_at->format('Y-m-d H:i') }}</td>
             <td class="px-4 py-2 text-right">
               <a href="{{ route('departmentsUser.edit', $dep) }}"

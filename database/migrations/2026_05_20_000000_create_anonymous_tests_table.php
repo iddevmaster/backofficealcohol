bbefaa272de93a2781fd44201960016fc1d5051e
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-05-20 15:41:14 +0700

    Update 2026_05_20_000000_create_anonymous_tests_table.php

--- database/migrations/2026_05_20_000000_create_anonymous_tests_table.php
+++ database/migrations/2026_05_20_000000_create_anonymous_tests_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('anonymous_tests', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->foreignId('org_id')->constrained('organizations')->cascadeOnDelete();
             $table->string('device_id');
             $table->string('user_id'); // Dummy user ID (e.g. 10-digit random string/number)
